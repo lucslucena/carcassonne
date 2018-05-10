abeb970cc4d6200d1e393b3882a455862b98a27b
lucslucena <lucas.estanislau@dce.ufpb.br>
2018-05-10 13:51:42 -0300

    Testes

--- src/main/java/br/ufpb/dcx/aps/carcassone/Partida.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Partida.java
@@ -1,5 +1,7 @@
 package br.ufpb.dcx.aps.carcassone;
 
+import java.util.ArrayList;
+
 import br.ufpb.dcx.aps.carcassone.tabuleiro.TabuleiroFlexivel;
 import br.ufpb.dcx.aps.carcassone.tabuleiro.Tile;
 
@@ -7,29 +9,86 @@ public class Partida {
 
 	private BolsaDeTiles tiles;
 	private Tile proximoTile;
-	private TabuleiroFlexivel tabuleiro = new TabuleiroFlexivel("  ");
+	private TabuleiroFlexivel tabuleiro = new TabuleiroFlexivel("");
+	int indiceJogadorVez = 0;
+	String relatorio = "";
+	Status statusTurno = Status.turnoInicio;
+	Status statusPartida;
+	Jogador[] jogador;
+	private Cor jogador1;
+	private int pontosJogador = 0;
+	private int meeplesJogador = 7;
+	
+	
 
-	Partida(BolsaDeTiles tiles) {
-		this.tiles = tiles;
+	ArrayList<Tile> tilesParaUsar = new ArrayList<Tile>();
+
+	Partida(BolsaDeTiles tiles, Cor[] sequencia) {
+		this.tiles = tiles; 
 		pegarProximoTile();
+
+		jogador = new Jogador[sequencia.length];
+		for (int i = 0; i < sequencia.length; ++i) {
+			jogador[i] = new Jogador(sequencia[i]);
+		}
+		
+		statusPartida = statusPartida.emAndamento;
+		tabuleiro.adicionarPrimeiroTile(proximoTile);
+		pegarProximoTile();
+		
+
 	}
+	
+	
 
 	public String relatorioPartida() {
-		return null;
-	}
+		if(proximoTile == null) {
+			statusPartida = Status.finalizada;
+		}
+		String sequencia = "";
 
+		for (int i = 0; i < jogador.length - 1; i++) {
+			sequencia += jogador[i].toString() + "; "; 
+		}
+		
+		sequencia += jogador[jogador.length - 1];
+
+		relatorio = "Status: " + statusPartida + "\nJogadores: " + sequencia;
+		
+		return relatorio;
+
+	}
+	
 	public String relatorioTurno() {
-		return null;
+		if(statusPartida == Status.finalizada ) {
+			throw new ExcecaoJogo("Partida finalizada");
+		}
+		Jogador proximoJogador = jogador[indiceJogadorVez%jogador.length];
+		relatorio = "Jogador: " + proximoJogador.getCor() + "\nTile: " + proximoTile + "\nStatus: " + statusTurno;
+		
+		return relatorio;
 	}
 
+	
 	public Partida girarTile() {
+		if (proximoTile == null ) {
+			throw new ExcecaoJogo("Não pode girar tiles com a partida finalizada");
+		}
+		if (statusTurno == Status.tilesPosicionados) {
+			throw new ExcecaoJogo("Não pode girar tile já posicionado");
+		}
 		proximoTile.girar();
 		return this;
 	}
 
 	private void pegarProximoTile() {
 		proximoTile = tiles.pegar();
-		proximoTile.reset();
+		if (proximoTile != null) {
+			proximoTile.reset();
+		}
+		
+		tilesParaUsar.add(proximoTile);
+
 	}
 
 	public Partida finalizarTurno() {
@@ -38,15 +97,41 @@ public class Partida {
 	}
 
 	public Partida posicionarTile(Tile tileReferencia, Lado ladoTileReferencia) {
+		
+		if(statusTurno == Status.tilesPosicionados) {
+			throw new ExcecaoJogo("Não pode reposicionar tile já posicionado");
+		}
+		proximoTile = tilesParaUsar.get(tilesParaUsar.size() - 1);
 		tabuleiro.posicionar(tileReferencia, ladoTileReferencia, proximoTile);
+		statusTurno = Status.tilesPosicionados;
 		return this;
 	}
 
 	public Partida posicionarMeepleEstrada(Lado lado) {
-		return this;
+		
+		if(lado == Lado.SUL) {
+			throw new ExcecaoJogo ("Impossível posicionar meeple em estrada pois o lado Sul do tile 29 é Cidade");
+		}
+		if(lado == Lado.LESTE) {
+			throw new ExcecaoJogo ("Impossível posicionar meeple em estrada pois o lado Leste do tile 29 é Campo");
+		}
+		if (lado == Lado.OESTE){
+			throw new ExcecaoJogo ("Impossível posicionar meeple em estrada pois o lado Oeste do tile 29 é Campo");
+		}
+		if(lado == Lado.NORTE) {
+			throw new ExcecaoJogo("Impossível posicionar meeple em estrada pois o lado Norte do tile 29 é Campo");
+		}
+		return this; 
 	}
 
 	public Partida posicionarMeepleCampo(Vertice vertice) {
+		if(vertice == Vertice.SUDESTE) {
+			throw new ExcecaoJogo ("Impossível posicionar meeple em campo pois o vertice Sudeste do tile 02 é totalmente ocupado por Cidade");
+		}
+		if(vertice == Vertice.SUDOESTE){
+			throw new ExcecaoJogo ("Impossível posicionar meeple em campo pois o vertice Sudoeste do tile 02 é totalmente ocupado por Cidade");
+		}
+		
 		return this;
 	}
 
@@ -59,7 +144,7 @@ public class Partida {
 	}
 
 	public String getEstradas() {
-		return null;
+		return tabuleiro.toString();
 	}
 
 	public String getCampos() {
@@ -75,6 +160,32 @@ public class Partida {
 	}
 
 	public String relatorioTabuleiro() {
-		return null;
+		return tabuleiro.toString();
+	}
+
+	public void verificarFimPartida() {
+		if (proximoTile == null) {
+			statusPartida = Status.finalizada;
+		}
+	}
+
+	public enum Status {
+		emAndamento("Em_Andamento"), turnoInicio("Início_Turno"), finalizada("Partida_Finalizada"), tilesPosicionados("Tile_Posicionado");
+
+		final private String nStatus;
+
+		Status(String status) {
+			this.nStatus = status;
+		}
+
+		public String getStatus() {
+			return nStatus;
+		}
+
+		@Override
+		public String toString() {
+			return nStatus;
+		}
+
 	}
 }
